47e2c57ebf4bd386539fa88a9e22c8e7580babd2
kotani kazumi <sbs@kkotani.net>
2022-03-07 09:24:20 +0900

    2022.3.7

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -4,6 +4,6 @@ from .views import BlogList, BlogDetail, BlogCreate, BlogDelete
 urlpatterns = [
     path('list/', BlogList.as_view(), name='list'),
     path('detail/<int:pk>/', BlogDetail.as_view(), name='detail'),
-    path('create/', BlogCreate.as_view(), name='create'),
-    path('delete/', BlogDelete.as_view(), name='delete')
+    path('create/<int:pk>/', BlogCreate.as_view(), name='create'),
+    path('delete/<int:pk>/', BlogDelete.as_view(), name='delete')
 ]
